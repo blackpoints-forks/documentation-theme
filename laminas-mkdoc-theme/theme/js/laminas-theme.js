dc8d81216c4a2f1bc8d5a08340a32d9f72a612bc
Eric Tobler <eric.tobler@blackpoints.ch>
2020-12-29 14:45:56 +0100

    toc, css for titles and mobile

--- laminas-mkdoc-theme/theme/js/laminas-theme.js
+++ laminas-mkdoc-theme/theme/js/laminas-theme.js
@@ -1,47 +1,69 @@
 $(function () {
-    setSidebarAndIndexPosition();
+    // sidebar - scroll to active navigation chapter (if not visible)
+    if (
+        $(".sidebar .subnavigation__list-item--active").length &&
+        $(".sidebar .subnavigation__list-item--active").closest(
+            "ul.subnavigation__list"
+        ).closest("li").length
+    ) {
+        var sidebarHeight = $(".sidebar").height();
+        var sidebarTopPosition = $(".sidebar").offset().top;
+        var activeMenuTopPosition = $(
+            ".sidebar .subnavigation__list-item--active"
+        )
+            .closest("ul.subnavigation__list")
+            .closest("li")
+            .offset().top;
+        if (activeMenuTopPosition > sidebarHeight + sidebarTopPosition) {
+            $(".sidebar").scrollTop(activeMenuTopPosition - sidebarTopPosition);
+        }
+    }
+    // sidebar and header position
+    setSidebarAndTocPosition();
     $(window).scroll(function () {
         stickyHeader();
-        setSidebarAndIndexPosition();
+        setSidebarAndTocPosition();
     });
     $(window).resize(function () {
-        setSidebarAndIndexPosition();
+        setSidebarAndTocPosition();
     });
 });
+
 /**
- * calculate position for sidebar and index
+ * Calculate position for sidebar and index
  */
-function setSidebarAndIndexPosition() {
+function setSidebarAndTocPosition() {
     // top position
-    var mainPositionTop =
-        $("[role=main]").offset().top - $(window).scrollTop();
-    var headerHeight = $('.header').innerHeight();
-    mainPositionTop = mainPositionTop < headerHeight ? headerHeight : mainPositionTop; // min value = headerHeight
-    $(".sidebar, .index__container").css("top", mainPositionTop);
+    var mainPositionTop = $("[role=main]").offset().top - $(window).scrollTop();
+    var headerHeight = $(".header").innerHeight();
+    mainPositionTop =
+        mainPositionTop < headerHeight ? headerHeight : mainPositionTop; // min value = headerHeight
+    $(".sidebar, .toc__container").css("top", mainPositionTop);
     // bottom position
-    var footerPositionTop =
-        $(".footer").offset().top - $(window).scrollTop();
+    var footerPositionTop = $(".footer").offset().top - $(window).scrollTop();
     var windowHeight = $(window).height();
     var sidebarPositionBottom = windowHeight - footerPositionTop;
-    sidebarPositionBottom = sidebarPositionBottom < 0 ? 0 :  sidebarPositionBottom; // min value = 0
+    sidebarPositionBottom =
+        sidebarPositionBottom < 0 ? 0 : sidebarPositionBottom; // min value = 0
     $(".sidebar").css("bottom", sidebarPositionBottom);
-    // right position for index
-    var indexRightPosition = parseInt($(".content").css('margin-left')) - $(".index__container").width();
-    $(".index__container").css("right", indexRightPosition);
- }
-
+    // right position for toc
+    var tocRightPosition =
+    (parseInt($("[role=main] > .container").css("margin-left")) +
+        parseInt($(".content").css("margin-left"))) -
+        $(".toc__container").width();
+    $(".toc__container").css("right", tocRightPosition);
+}
 
 /**
- * Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+ * Sticky header
  */
- function stickyHeader() {
-    var headerPositionTop = $('.header').offset().top;
-    if ($(window).scrollTop() >= $('.navbar').innerHeight()) {
-        $('.header').addClass("sticky");
+function stickyHeader() {
+    var headerPositionTop = $(".header").offset().top;
+    if ($(window).scrollTop() >= $(".navbar").innerHeight()) {
+        $(".header").addClass("sticky");
         $('[role="main"]').addClass("padding-sticky");
-
     } else {
-        $('.header').removeClass("sticky");
+        $(".header").removeClass("sticky");
         $('[role="main"]').removeClass("padding-sticky");
     }
 }
